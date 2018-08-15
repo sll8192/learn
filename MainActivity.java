7cab5610b8a62102065a73b79cf8026e70c70e04
sunlulu <sunll@guahao.com>
2018-08-15 15:47:03 +0800

    第二次修改maincctivity

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,8 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 //这是我的Git学习测试
+
+//第二次更改这个文件的的代码
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
